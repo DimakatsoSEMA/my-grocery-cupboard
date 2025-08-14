5b3a1b8471abf101e06c56f8be0df70ab2ae2adf
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-14 14:44:43 +0200

    added models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Location(models.TextChoices):
+    FRIDGE = 'Fridge'
+    FREEZER = 'Freezer'
+    CUPBOARD = 'Cupboard'
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=50)  # e.g., grams, ml, pieces
+    location = models.CharField(max_length=50, choices=Location.choices)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    expiry_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
