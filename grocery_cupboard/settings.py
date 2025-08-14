de6188b00a22df5d9eca00311eaf8d13ab9f9d87
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-14 14:48:17 +0200

    registered apps

--- grocery_cupboard/settings.py
+++ grocery_cupboard/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inventory',
+    'meals',
+    'users',
 ]
 
 MIDDLEWARE = [
