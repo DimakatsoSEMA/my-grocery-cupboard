b88f23ea78ebd5907a0468a599e0525cd6a87a54
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-14 14:46:17 +0200

    added models.py

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from inventory.models import Item
 
-# Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField(Item, through='MealIngredient')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class MealIngredient(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity_needed = models.FloatField()
+    unit = models.CharField(max_length=50)
