25e14b88a0e8e346bf1319831e8f71cf3b371b76
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-16 14:43:55 +0200

    defined views

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,4 @@ from .views import ItemListCreateView, ItemRetrieveUpdateDeleteView
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name='item-list-create'),
     path('items/<int:pk>/', ItemRetrieveUpdateDeleteView.as_view(), name='item-detail'),
-    path('', views.index, name='inventory-home')
 ]
