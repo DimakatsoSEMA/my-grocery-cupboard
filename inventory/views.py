e6ab523febdcef07c8dd48b6f53418e135bc14a8
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-16 14:06:19 +0200

    created CRUD API views

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class ItemRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
