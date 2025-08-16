c198f2f162c20451f80a58ad231ad8aad5d40e39
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-16 14:36:12 +0200

    edited urls.py and models.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ItemListCreateView, ItemRetrieveUpdateDeleteView
+from .views import views, ItemListCreateView, ItemRetrieveUpdateDeleteView
 
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name='item-list-create'),
     path('items/<int:pk>/', ItemRetrieveUpdateDeleteView.as_view(), name='item-detail'),
+    path('', views.index, name='inventory-home')
 ]
