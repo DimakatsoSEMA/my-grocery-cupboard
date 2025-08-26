ef54ed56efaa2d7bd7f8d4448813cdb4cea5059b
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-26 13:17:44 +0200

    updated models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings  
 
 class Location(models.TextChoices):
     FRIDGE = 'Fridge'
@@ -11,9 +11,13 @@ class Item(models.Model):
     quantity = models.FloatField(default=0)
     unit = models.CharField(max_length=50)  # e.g., grams, ml, pieces
     location = models.CharField(max_length=50, choices=Location.choices)
-    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  
+        on_delete=models.CASCADE
+    )
     expiry_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
