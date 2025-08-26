c78831647cda2f722951701d2a00ba9ae2706e73
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-26 12:38:27 +0200

    updated models.py for users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Example extra field
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ('admin', 'Admin'),
+            ('member', 'Member'),
+        ],
+        default='member'
+    )
+
+    def __str__(self):
+        return self.username
+
