85b3c0c59f84c3e1dda695ffb20056489141fcea
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-26 14:05:09 +0200

    added urls.py

--- grocery_cupboard/urls.py
+++ grocery_cupboard/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
     path("api/users/", include("users.urls")),
+    path("api/auth/login/", obtain_auth_token, name="api-token-auth"),
 ]
