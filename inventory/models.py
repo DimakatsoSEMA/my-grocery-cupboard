d5e1ae6adb97386925b81d1623aade79c0844673
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-27 14:41:51 +0200

    updated low stock alert

--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings  
+from django.conf import settings
 
 class Location(models.TextChoices):
     FRIDGE = 'Fridge'
@@ -12,12 +12,18 @@ class Item(models.Model):
     unit = models.CharField(max_length=50)  # e.g., grams, ml, pieces
     location = models.CharField(max_length=50, choices=Location.choices)
     added_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,  
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
     expiry_date = models.DateField(null=True, blank=True)
+    low_stock_threshold = models.FloatField(default=5)
 
     def __str__(self):
         return self.name
+
+    @property
+    def is_low_stock(self):
+        return self.quantity < self.low_stock_threshold
+
 
 
