7cd7eb60043ec3a02359042294f667f7b73b809a
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-27 12:32:22 +0200

    updated serializers.py to show ingredients

--- meals/serializers.py
+++ meals/serializers.py
@@ -1,29 +1,19 @@
 from rest_framework import serializers
 from .models import Meal, MealIngredient
-from inventory.serializers import ItemSerializer
+from inventory.models import Item
 
 class MealIngredientSerializer(serializers.ModelSerializer):
-    item = ItemSerializer(read_only=True)
-    item_id = serializers.PrimaryKeyRelatedField(
-        queryset=MealIngredient.objects.none(),  # prevent global queries
-        source='item',
-        write_only=True
-    )
+    item_name = serializers.CharField(source='item.name', read_only=True)
 
     class Meta:
         model = MealIngredient
-        fields = ['id', 'item', 'item_id', 'quantity_needed', 'unit']
+        fields = ['id', 'item', 'item_name', 'quantity_needed', 'unit']
 
 
 class MealSerializer(serializers.ModelSerializer):
     ingredients = MealIngredientSerializer(source='mealingredient_set', many=True, read_only=True)
-    is_favorite = serializers.BooleanField(default=False)  
 
     class Meta:
         model = Meal
         fields = ['id', 'name', 'ingredients', 'created_by', 'is_favorite']
         read_only_fields = ['created_by']
-
-    def create(self, validated_data):
-        validated_data['created_by'] = self.context['request'].user
-        return super().create(validated_data)
