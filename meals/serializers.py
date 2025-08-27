bbdd913b8887374e478eda00122a9de1b08bf6ff
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-27 12:45:24 +0200

    updated serializers.py

--- meals/serializers.py
+++ meals/serializers.py
@@ -4,16 +4,27 @@ from inventory.models import Item
 
 class MealIngredientSerializer(serializers.ModelSerializer):
     item_name = serializers.CharField(source='item.name', read_only=True)
+    item = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
 
     class Meta:
         model = MealIngredient
-        fields = ['id', 'item', 'item_name', 'quantity_needed', 'unit']
-
 
 class MealSerializer(serializers.ModelSerializer):
-    ingredients = MealIngredientSerializer(source='mealingredient_set', many=True, read_only=True)
+    ingredients = MealIngredientSerializer(many=True)  
 
     class Meta:
         model = Meal
         fields = ['id', 'name', 'ingredients', 'created_by', 'is_favorite']
         read_only_fields = ['created_by']
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients', [])
+        validated_data['created_by'] = self.context['request'].user
+        meal = Meal.objects.create(**validated_data)
+
+        for ingredient_data in ingredients_data:
+            MealIngredient.objects.create(meal=meal, **ingredient_data)
+
+        return meal
+        fields = ['id', 'item', 'item_name', 'quantity_needed', 'unit']
+
