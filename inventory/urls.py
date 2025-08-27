4e27c2b86785a6ad1af064a10fb438d306751cf8
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-27 14:45:22 +0200

    added grocery list API

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ItemListCreateView, ItemRetrieveUpdateDeleteView
+from .views import (
+    ItemListCreateView, 
+    ItemRetrieveUpdateDeleteView,
+    grocery_list
+)
 
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name='item-list-create'),
     path('items/<int:pk>/', ItemRetrieveUpdateDeleteView.as_view(), name='item-detail'),
+    path('grocery-list/', grocery_list, name='grocery-list'),  
 ]
