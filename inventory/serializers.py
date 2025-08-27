c5b399d227c758673e0c2b000fa5a45931dcbdf7
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-27 14:42:47 +0200

    added low stock alert

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
+    low_stock = serializers.SerializerMethodField()
+
     class Meta:
         model = Item
         fields = '__all__'
+
+    def get_low_stock(self, obj):
+        return obj.is_low_stock
