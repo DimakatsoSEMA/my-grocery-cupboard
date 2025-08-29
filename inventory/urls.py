e0cd66cb617fd5e49b664c68a5f031f038f0912b
DimakatsoSEMA <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-08-29 09:09:56 +0200

    updated urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import (
     ItemListCreateView, 
     ItemRetrieveUpdateDeleteView,
-    grocery_list
+    grocery_list,
+    item_list_view,
+    grocery_list_view
 )
 
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name='item-list-create'),
     path('items/<int:pk>/', ItemRetrieveUpdateDeleteView.as_view(), name='item-detail'),
-    path('grocery-list/', grocery_list, name='grocery-list'),  
+    path('grocery-list/', grocery_list, name='grocery-list'),
+    path('items-ui/', item_list_view, name='item-list'),
+    path('grocery-list-ui/', grocery_list_view, name='grocery-list-view'),
 ]
